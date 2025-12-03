6a9fd9fc64d15bfee7d1e2793fd01bc57dc100c0
jinnk0 <jin8073@gmail.com>
2025-12-03 09:34:02 +0900

    feat: 사용자 등록 및 인증을 위한 기본 dto 정의 (#3) - 회원가입 요청 dto - 회원가입 후 사용자 정보 반환 dto - 로그인 요청 dto - access, refresh token 반환 dto

---
+++ src/main/java/org/bf/userservice/application/dto/UserResponse.java
@@ -0,0 +1,11 @@
+package org.bf.userservice.application.dto;
+
+import java.util.UUID;
+
+public record UserResponse(
+        UUID userId,
+        String username,
+        String email,
+        String nickname
+) {
+}
